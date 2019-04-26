838731750e8b637761ed08308e380357ec2a0218
WzcTHU <zhengche16@mails.tsinghua.edu.cn>
2019-04-26 16:14:16 +0800

    4/26

--- Period2/Search.py
+++ Period2/Search.py
@@ -1,22 +1,49 @@
-import json
+import json 
+import os
 
 class Search():
     def __init__(self):
         self.total_doc = 3204
         self.target_words_list = []
         self.RevIndexDic = {}
+        self.doc_list = []
+
+    def GetAllDoc(self, dirname='/Period1/cacm'):
+        # last_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+        cur = os.getcwd()
+        temp_list = os.listdir(cur + dirname)
+        for each in temp_list:
+            self.doc_list.append(each[5:9])
+        return self.doc_list
     
     def GetInput(self):
         print('Please input words or sentence to search in database: ')
         print('(Enter q to stop enter and begin search)')
+        temp_list = []
         while(1):
             word = input()
             if(word == 'q'):
                 break
             else:
-                self.target_words_list.extend(word.split(' '))
+                temp_list.extend(word.split(' '))
+                # self.target_words_list.extend(word.split(' '))
+        for each in temp_list:
+            if(each.lower() not in self.target_words_list):
+                self.target_words_list.append(each.lower())
+        return self.target_words_list
         
     def LoadRevIndex(self):
         with open('RevIndex.txt', 'r') as json_file:
             self.RevIndexDic = json.load(json_file)
+        return self.RevIndexDic
+
     
+
+    
+    
+if __name__ == '__main__':
+    m_s = Search()
+    # li = m_s.GetInput()
+    # dic = m_s.LoadRevIndex()
+    # print(li)
+    m_s.GetAllDoc()
