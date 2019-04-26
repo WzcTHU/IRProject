c4ff7b4b05e8e0872a998934d20524204c6d12c9
WzcTHU <zhengche16@mails.tsinghua.edu.cn>
2019-04-26 22:31:19 +0800

    4/26

--- Period2/Search.py
+++ Period2/Search.py
@@ -1,5 +1,6 @@
 import json 
 import os
+import math as m
 
 class Search():
     def __init__(self):
@@ -13,7 +14,7 @@ class Search():
         self.words_p_dic = {}               # 存储每个单词的p值
 
 
-    def GetAllDoc(self, dirname=r'\\Period1\\cacm'):
+    def __GetAllDoc(self, dirname=r'\\Period1\\cacm'):
         last_dir = os.path.abspath(os.path.dirname(os.getcwd()))
         # cur = os.getcwd()
         temp_list = os.listdir(last_dir + dirname)
@@ -37,7 +38,7 @@ class Search():
                 self.target_words_list.append(each.lower())
         return self.target_words_list
         
-    def LoadRevIndex(self):
+    def __LoadRevIndex(self):
         with open('RevIndex.txt', 'r') as json_file:
             self.rev_index_dic = json.load(json_file)
         self.all_words_list = list(self.rev_index_dic.keys())
@@ -45,15 +46,32 @@ class Search():
         return self.rev_index_dic
 
     def CalPR1st(self):
-        self.LoadRevIndex()
-        self.GetAllDoc()
+        self.__LoadRevIndex()
+        self.__GetAllDoc()
         for word in self.all_words_list:
             self.words_p_dic[word] = 0.5        # 使用初始P值
             self.words_r_dic[word] = len(self.rev_index_dic[word]['files']) / self.total_doc_num
         return self.words_p_dic, self.words_r_dic
 
+    def CalSim1st(self):
+        for each_doc in self.doc_list:
+            self.sim_dic[each_doc] = 0
+            for each_word in self.target_words_list:
+                if each_word in self.rev_index_dic.keys():      # 该索引词在词库中才进行计算
+                    if(each_doc in self.rev_index_dic[each_word]['files'].keys()):  # 文档含有该词
+                        pi = self.words_p_dic[each_word]
+                        ri = self.words_r_dic[each_word]
+                        self.sim_dic[each_doc] += m.log10((pi / (1 - pi)) * ((1 - ri) / ri))
+            
+        top_doc_list = sorted(self.sim_dic.items(), key=lambda x:x[1], reverse=True)
+        print(top_doc_list)
+
+
+
     
 if __name__ == '__main__':
     m_s = Search()
+    m_s.GetInput()
     _, rd = m_s.CalPR1st()
-    print(rd['language'])
+    m_s.CalSim1st()
+    # print(rd['language'])
